7cf237904fac241a4b76bf259991a5b43f400594
Javier Gonzalez <javiergonzaleze98@gmail.com>
2024-07-09 11:20:58 -0400

    prod

--- frontend/src/app/lib/conn/conn.ts
+++ frontend/src/app/lib/conn/conn.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export async function GetReview(review: string){
-    const data = await axios.post("http://localhost:8000/api/review/",{review})
+    const data = await axios.post("http://3.144.171.135:8000/api/review/",{review})
     if(data.status == 200){
         return data.data
     }
